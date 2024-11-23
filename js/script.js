0c1e89158a59a9e1f2d0940464591387af9305bf
CyberIndo <55630381+zmanias@users.noreply.github.com>
2024-11-24 00:05:52 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+document.getElementById('payButton').addEventListener('click', function () {
+    const price = this.getAttribute('data-price');
 
+    fetch('api.php', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `amount=${price}`
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                alert('Terjadi kesalahan: ' + data.error);
+            } else {
+                window.location.href = data.payment_url; // URL QRIS
+            }
+        })
+        .catch(error => {
+            alert('Terjadi kesalahan: ' + error.message);
+        });
+});
